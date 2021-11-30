76a40264f1eb2e46acff06db4c601e85b8ff4819
Marcus <marcusclemmons1@gmail.com>
2021-11-29 21:25:34 -0500

    all done

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ishaklhsa'
+
+debug = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def ask_questions():
+    """Makes form to ask words."""
+    prompts = story.prompts
+    return render_template("questions.html", prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+    """Shows stories"""
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
